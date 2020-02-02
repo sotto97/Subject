6539c8e6d708e5de713bfa1f264654409dc928a7
sotto97 <sotto.bw97@gmail.com>
2020-02-02 20:30:35 +0900

    [Finish] 3-8

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key='8eb52255f66e18d3a424abec57368f0ea6f05fb9de8e97b1228598c74f6ecd42bf391e297555439d3e5314e0b6f9a011bb4de8f6dc2d16e322f0c6b920ca78d5'
