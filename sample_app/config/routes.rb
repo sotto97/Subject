ba5dd7047ba4fce09342672141551bb4cb262ef3
sotto97 <sotto.bw97@gmail.com>
2020-02-02 19:22:06 +0900

    [Finish] 3-6

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   get 'top'=>'homes#top'
   post 'todolists'=>'todolists#create'
   get 'todolists'=>'todolists#index'
-
-  get 'todolists/:id'=>'todolists#show', as:'todolist'
-  # .../todolists/1 や .../todolists/3 に該当する
-
+	get 'todolists/:id'=>'todolists#show', as:'todolist'
+	get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
+  patch 'todolists/:id'=>'todolists#update', as:'update_todolist'
 end
