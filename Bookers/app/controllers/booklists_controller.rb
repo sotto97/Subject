33562267d1451c960e5ccaa684491b5db7a02a92
sotto97 <sotto.bw97@gmail.com>
2020-02-04 16:20:28 +0900

    [add] Bookers

---
+++ Bookers/app/controllers/booklists_controller.rb
@@ -0,0 +1,19 @@
+class BooklistsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
---
+++ Bookers/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'booklists/index'
+  get 'booklists/show'
+  get 'booklists/new'
+  get 'booklists/edit'
+  get 'booklists/create'
+  get 'booklists/destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
