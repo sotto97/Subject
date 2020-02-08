9b5b90d4bc10110746e606c9227dcb23f77baeca
sotto97 <sotto.bw97@gmail.com>
2020-02-08 17:10:19 +0900

    [Add] meshiterro

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key='568709e701a20e8e03e00c14229bbe8d78a39516fc6037424e0cd3e3abe60f8955bcd12cbe02912f2f8e39e952e1f8f6274a9fe275fb0ae739a7594b6320157e'
